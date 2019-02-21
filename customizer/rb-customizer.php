8efe1686752643567780f10102a399899fdf0b4b
Juan Rabaglia <raba_emp@hotmail.com>
2019-02-21 17:37:24 -0300

    Fix jquery bug

--- customizer/rb-customizer.php
+++ customizer/rb-customizer.php
@@ -36,7 +36,7 @@ add_action( 'customize_controls_enqueue_scripts', function () use ($rb_customize
 // Customizer SCRIPTS
 // =============================================================================
 add_action( 'customize_controls_enqueue_scripts', function () use ($rb_customizer_uri){
-    wp_enqueue_script( "jquery-3", "https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js", true );
+    //wp_enqueue_script( "jquery-3", "https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js", true );
     wp_enqueue_script( "jquery-ui", $rb_customizer_uri . '/js/jquery-ui-1.12.1.custom/jquery-ui.min.js', array("jquery"), true );
     wp_enqueue_script( 'rb-wp-editor-customizer', $rb_customizer_uri . '/js/rb-customizer-panel.js', array( 'jquery' ), rand(), true );
     wp_enqueue_script( "rb-customizer-image-selection-control", $rb_customizer_uri."/js/rb-customizer-image-selection-control.js", array("jquery"), true );
