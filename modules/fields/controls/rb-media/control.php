4bd1561be906734fde2571b80d93fba7ede3690c
Juan Cruz Rabaglia <juanrabaa@gmail.com>
2020-07-21 18:45:13 -0300

    Added a parameter for the attachment control that allows to change the value from the attachment that is stores. The setting is `store_value` and the posible values are: - `id`: Stores the attachment ID - `url`: Stores the attachment image url

--- modules/fields/controls/rb-media/control.php
+++ modules/fields/controls/rb-media/control.php
@@ -4,11 +4,12 @@ class RB_Media_Control extends RB_Field_Control{
 
     public function render_content(){
         extract($this->settings);
+
         ?>
         <?php $this->print_control_header(); ?>
-        <div class="inputs-generator-inputs-holder">
+        <div class="inputs-generator-inputs-holder" data-value-type="<?php echo esc_attr( $this->get_value_type() ); ?>">
             <div class="input-wp-media-image-holder">
-                <img class="input-image-src" src="<?php echo esc_attr($this->value); ?>">
+                <img class="input-image-src" src="<?php echo esc_attr( $this->get_img_url() ); ?>">
                 <div class="input-image-placeholder">
                 <p> Select an image </p>
                 </div>
@@ -17,6 +18,21 @@ class RB_Media_Control extends RB_Field_Control{
             <div class="remove-image-button"><i class="fas fa-times" title="Remove image"></i></div>
         </div>
         <?php
+    }
+
+    public function get_img_url(){
+        if(!$this->value)
+            return '';
+        if($this->get_value_type() == 'id'){
+            $value = intval($this->value);
+            $src = $value ? wp_get_attachment_url($value) : '';
+            return $src ? $src : '';
+        }
+        return $this->value;
+    }
+
+    public function get_value_type(){
+        return isset($this->settings['store_value']) ? $this->settings['store_value'] : 'url';
     }
 
 }
--- modules/fields/controls/rb-media/js/rb-media-control.js
+++ modules/fields/controls/rb-media/js/rb-media-control.js
@@ -1,5 +1,11 @@
 (function($){
 
+    function get_attachment_value($control, attachment){
+        if($control.attr('data-value-type') == 'id')
+            return attachment.id;
+        return attachment.url;
+    }
+
     $(document).ready(function(){
         $(document).on('click', '.input-wp-media-image-holder', function(e) {
             e.stopPropagation();
@@ -8,8 +14,8 @@
                 return;
             var $button = $(this);
 			$button.attr('media-open', '');
-            console.log( $(this));
-            var $controlPanel = $(this).closest(".customize-control-multiple-inputs");
+            // console.log( $(this));
+            var $control = $(this).closest(".inputs-generator-inputs-holder");
             var $input_field = $(this).find("input");
             var $image_holder = $(this).find(".input-image-src");
             var custom_uploader = wp.media.frames.file_frame = wp.media({
@@ -19,14 +25,26 @@
                 },
                 multiple: false
             });
+
+            custom_uploader.on('open', function() {
+                if($control.attr('data-value-type') == 'id' && $input_field.val()){
+                    // Used for defining the image selections in the media library.
+                    var selectionAPI = custom_uploader.state().get( 'selection' );
+                    var attachment = wp.media.attachment( $input_field.val() );
+                    selectionAPI.add( attachment ? [ attachment ] : []);
+                }
+            });
+
             custom_uploader.on('select', function() {
                 var attachment = custom_uploader.state().get('selection').first().toJSON();
-                console.log(attachment.url);
-                $input_field.val(attachment.url).trigger('input');
+                const storeValue = get_attachment_value($control, attachment);
+                // console.log(attachment.url);
+                $input_field.val(storeValue).trigger('input');
                 //updateValue( $controlPanel );
                 $image_holder.attr('src', attachment.url );
                 $button.removeAttr('media-open');
             });
+
             custom_uploader.open();
         });
 
@@ -36,12 +54,11 @@
         });
 
         function emptyImageInput( $inputHolder ){
-    		var $controlPanel = $inputHolder.closest(".customize-control-multiple-inputs");
     		var $image = $inputHolder.find('.input-image-src');
     		var $input = $inputHolder.find('input');
     		$image.attr('src','');
     		$input.val('').trigger('input');
-    		console.log($controlPanel);
+    		// console.log($controlPanel);
     		//updateValue( $controlPanel );
     	}
     });
