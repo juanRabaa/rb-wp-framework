1dd017a4c64cf917307a729906d1e360b9627a13
Juan Cruz Rabaglia <juanrabaa@gmail.com>
2020-09-11 10:32:52 -0300

    Search module custom post type search template fixed. Uses global post type and checks for is_search.

--- modules/fields/controls/rb-select-image/scripts.php
+++ modules/fields/controls/rb-select-image/scripts.php
@@ -1,5 +1,4 @@
 <?php
-var_dump(rb_get_file_url(__FILE__));
 //Image Selection control
 wp_enqueue_style( 'rb-image-selection-control', rb_get_file_url(__FILE__) . '/css/rb-image-selection-control.css' );
 wp_enqueue_script( 'rb-select-image', rb_get_file_url(__FILE__) . '/js/rb-select-image-control.js', array('jquery'), true );
--- modules/search/module.php
+++ modules/search/module.php
@@ -9,14 +9,15 @@ class RB_Search_Module extends RB_Framework_Module{
         if(self::$initialized)
             return false;
         add_action( 'template_include', array(self::class, 'search_redirect') );
-        add_filter( 'pre_get_posts', array(self::class, 'set_search_query_post_type') );
+        //deprecated
+        //add_filter( 'pre_get_posts', array(self::class, 'set_search_query_post_type') );
         return true;
     }
 
     /**
     *   Activates the use of custom posts type search templates.
-    *   Post type is specified in the search query string, in the post_type variable. Ex: ?s=test&post_type=post
-    *   Search templates must have the next format: search-$post_type.php
+    *   Global post type will be used
+    *   Search templates must have the next format: search-{$post_type}.php
     *   If the template is not found, search will fallback to the basic template search.php
     */
     static public function activate_post_type_search_template(){
@@ -27,9 +28,10 @@ class RB_Search_Module extends RB_Framework_Module{
         if(!self::$post_type_custom_search_template)
             return $template;
 
-        $post_type = isset($_GET['post_type']) ? $_GET['post_type'] : null;
+        $is_search = is_search();
+        $post_type = get_post_type();
 
-        if(is_string($post_type)){
+        if($is_search && is_string($post_type)){
             $new_template = locate_template( array( "search-$post_type.php" ) );
             if ( '' != $new_template ) {
                 return $new_template ;
@@ -39,6 +41,7 @@ class RB_Search_Module extends RB_Framework_Module{
         return $template;
     }
 
+    //deprecated
     static public function set_search_query_post_type( $query ) {
         if(!self::$post_type_custom_search_template)
             return $query;
